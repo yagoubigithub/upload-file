c369a559c0531bfc3e92e05856158cba42fe9d3e
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-07-04 15:51:59 +0100

    remove file and add files

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ export default class UploadFile extends Component {
     text: PropTypes.string
   };
 
+  state = {
+    files: []
+  };
+  onchange = file => {
+    const files = [...this.state.files];
+    files.push(file[0]);
+   
+    this.setState({ files });
+  };
+  removeFile = index =>{
+    const files = [...this.state.files];
+    files.splice(index, 1);
+    this.setState({ files });
+  }
+
   render() {
     const {
       accepts,
@@ -17,8 +32,6 @@ export default class UploadFile extends Component {
       maxFileSize,
       minFileSize,
       label,
-     
-
       color
     } = this.props;
 
@@ -29,50 +42,59 @@ export default class UploadFile extends Component {
           style={{ backgroundColor: color }}
         >
           <img src={PublishIcon} className={styles["svg-icon"]} />
-          <input type="file" style={{ display: "none" }} id="fileupload" />
+          <input
+            type="file"
+            onChange={ref => this.onchange(ref.target.files)}
+            style={{ display: "none" }}
+            id="fileupload"
+          />
           <label htmlFor="fileupload">{label}</label>
         </button>
         <div className={styles["files-container"]}>
-          <div className={styles["file"]}>
-          <span className={styles["file-name"]}>translate.txt</span>
-          <span className={styles["file-close"]}>
-          <img src={CloseIcon} />
-          </span>
+        {this.state.files.map((file,index)=>{
+          return(
+            <div className={styles["file"]} key={index}>
+            <span className={styles["file-name"]}>{file.name}</span>
+            <span className={styles["file-close"]} onClick={()=>this.removeFile(index)}>
+              <img src={CloseIcon} />
+            </span>
           </div>
+          );
+
+        })}
          
 
           <div className={styles["file"]}>
-          <span className={styles["file-name"]}>translate.txt</span>
-          <span className={styles["file-close"]}>
-          <img src={CloseIcon} />
-          </span>
-          </div>
-
-
-          <div className={styles["file"]}>
-          <span className={styles["file-name"]}>translate.txt</span>
-          <span className={styles["file-close"]}>
-            <img src={CloseIcon} />
-          </span>
+            <span className={styles["file-name"]}>translate.txt</span>
+            <span className={styles["file-close"]}>
+              <img src={CloseIcon} />
+            </span>
           </div>
 
           <div className={styles["file"]}>
-          <span className={styles["file-name"]}>translate.txt</span>
-          <span className={styles["file-close"]}>
-          <img src={CloseIcon} />
-          </span>
+            <span className={styles["file-name"]}>translate.txt</span>
+            <span className={styles["file-close"]}>
+              <img src={CloseIcon} />
+            </span>
+          </div>
+
+          <div className={styles["file"]}>
+            <span className={styles["file-name"]}>translate.txt</span>
+            <span className={styles["file-close"]}>
+              <img src={CloseIcon} />
+            </span>
           </div>
           <div className={styles["file"]}>
-          <span className={styles["file-name"]}>translate.txt</span>
-          <span className={styles["file-close"]}>
-          <img src={CloseIcon} />
-          </span>
+            <span className={styles["file-name"]}>translate.txt</span>
+            <span className={styles["file-close"]}>
+              <img src={CloseIcon} />
+            </span>
           </div>
           <div className={styles["file"]}>
-          <span className={styles["file-name"]}>translate.txt</span>
-          <span className={styles["file-close"]}>
-          <img src={CloseIcon} />
-          </span>
+            <span className={styles["file-name"]}>translate.txt</span>
+            <span className={styles["file-close"]}>
+              <img src={CloseIcon} />
+            </span>
           </div>
         </div>
       </div>
