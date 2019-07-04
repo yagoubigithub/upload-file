85eae6a1ddefcbfa2f32fb63cd3713dc607523aa
yagoubigithub <yagoubi.aek.2@gmail.com>
2019-07-04 14:14:39 +0100

    first design

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from '@yagoubi/upload-file'
+import UploadFile from '@yagoubi/upload-file'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+      <br />
+      <br />
+      <br />
+      <br />
+         <UploadFile   
+        accepts={['image/png', '.pdf', 'audio/*']}
+          multiple={false}
+          maxFiles={3}
+          maxFileSize={10000000}
+          minFileSize={0}
+          label={"upload images.."}
+          color="#F24"
+         />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
+import styles from "./styles.css";
+import PublishIcon from "./icons/publishIcon.svg";
+import CloseIcon from "./icons/close.svg";
+export default class UploadFile extends Component {
   static propTypes = {
     text: PropTypes.string
-  }
+  };
 
   render() {
     const {
-      text
-    } = this.props
+      accepts,
+      multiple,
+      maxFiles,
+      maxFileSize,
+      minFileSize,
+      label,
+
+      color
+    } = this.props;
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={styles["btn-upload-container"]}>
+        <button
+          className={styles["btn-upload"]}
+          style={{ backgroundColor: color }}
+        >
+          <img src={PublishIcon} className={styles["svg-icon"]} />
+          <input type="file" style={{ display: "none" }} id="fileupload" />
+          <label htmlFor="fileupload">{label}</label>
+        </button>
+        <div className={styles["files-container"]}>
+          <div className={styles["file"]}>
+          <span className={styles["file-name"]}>translate.txt</span>
+          <span className={styles["file-close"]}>
+            X
+          </span>
+          </div>
+          <div className={styles["file"]}>translate.txt</div>
+        </div>
       </div>
-    )
+    );
   }
 }
